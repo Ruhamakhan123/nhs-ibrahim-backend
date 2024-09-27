7a25d52cd667ccd4af740992836613a12b14b5b7
SubhanKhurshid <k213096@nu.edu.pk>
2024-09-27 22:56:23 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:3001", // Ensure this is updated or removed based on your production environment needs
+    origin: "https://ibrahim-medical-virid.vercel.app/", // Ensure this is updated or removed based on your production environment needs
     credentials: true,
   });
 
